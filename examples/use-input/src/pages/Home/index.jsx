a1b06b3e40e115d20bb844a93815a72c84350b46
fushen <fushen.jzw@alibaba-inc.com>
2020-10-09 14:14:46 +0800

    chore: lint

--- examples/use-input/src/pages/Home/index.jsx
+++ examples/use-input/src/pages/Home/index.jsx
@@ -14,23 +14,25 @@ export default function Home() {
 
   const onInputOne = (e) => {
     setValOne(e.value);
-  }
+  };
 
   const onInputTwo = (e) => {
     setValTwo(e.value);
-  }
+  };
 
   const onInputThree = (e) => {
     setValThreee(e.detail.value);
-  }
+  };
 
   const onInputFour = (e) => {
     setValFour(e.detail.value);
-  }
+  };
 
   return (
     <View className="home">
-      <View onClick={() => {setCount(Math.random())}}>{count}</View>
+      <View onClick={() => {
+        setCount(Math.random());
+      }}>{count}</View>
       <TextInput placeholder="rax-textinput 自动聚焦" value={valOne} autoFocus={true} onInput={onInputOne} />
       <TextInput placeholder="rax-textinput 非自动聚焦" value={valTwo} autoFocus={false} onInput={onInputTwo} />
 
