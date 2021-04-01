8ba749c5292331f40bd28ff89dde4aecffcaf0f5
逆葵 <xianyong.yxy@alibaba-inc.com>
2021-04-01 19:25:29 +0800

    test(miniapp): update test case

--- packages/miniapp-render/__tests__/util/tool.test.js
+++ packages/miniapp-render/__tests__/util/tool.test.js
@@ -16,20 +16,3 @@ test('tool: getId', () => {
   expect(tool.getId().toString()).toEqual('1');
   expect(tool.getId().toString()).toEqual('2');
 });
-
-test('tool: throttle/flushThrottleCache', async() => {
-  let count = 0;
-  const func = tool.throttle(() => count++);
-  func();
-  func();
-  func();
-  await mock.sleep(10);
-  expect(count).toBe(1);
-  func();
-  func();
-  expect(count).toBe(1);
-  tool.flushThrottleCache();
-  expect(count).toBe(2);
-  await mock.sleep(10);
-  expect(count).toBe(2);
-});
