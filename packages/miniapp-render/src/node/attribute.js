5debe4fc7e4915356fbe0786268bc6da50d15f78
逆葵 <xianyong.yxy@alibaba-inc.com>
2021-04-08 16:25:16 +0800

    feat(miniapp): optimize __hasExtraAttribute judge

--- packages/miniapp-render/src/node/attribute.js
+++ packages/miniapp-render/src/node/attribute.js
@@ -17,8 +17,8 @@ class Attribute {
         const datasetName = toCamel(name.substr(5));
         element.dataset[datasetName] = value;
       }
-      if (name !== 'id' && name !== 'class') {
-        this.__element.__hasExtraAttribute = true; // Indicates that the element has extra attributes besides id/style/class
+      if (!element.__hasExtraAttribute && name !== 'id' && name !== 'class') {
+        element.__hasExtraAttribute = true; // Indicates that the element has extra attributes besides id/style/class
       }
       const payload = {
         path: `${element._path}.${name}`,
