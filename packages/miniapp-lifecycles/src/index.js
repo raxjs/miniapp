53ff373c1ea134ef2eaadc3c887e1a97c315cfca
逆葵 <xianyong.yxy@alibaba-inc.com>
2021-07-22 14:47:49 +0800

    feat(miniapp-lifecycles): publish version 1.0.0

---
+++ packages/miniapp-lifecycles/src/index.js
@@ -0,0 +1,11 @@
+export function registerNativeEventListeners(Klass, events) {
+  // For rax miniapp runtime babel plugins prev compile
+}
+
+export function addNativeEventListener(eventName, callback) {
+  document.addEventListener(eventName, callback);
+}
+
+export function removeNativeEventListener(eventName, callback) {
+  document.removeEventListener(eventName, callback);
+}
