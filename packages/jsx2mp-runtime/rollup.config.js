1215e4e67cd6ace21f9c9c82eacb3bc13cfba5d4
逆葵 <xianyong.yxy@alibaba-inc.com>
2021-08-10 14:37:32 +0800

    fix: lint error

--- packages/jsx2mp-runtime/rollup.config.js
+++ packages/jsx2mp-runtime/rollup.config.js
@@ -45,7 +45,7 @@ function getBabelConfig({ platform = 'ali' }) {
     plugins: [
       '@babel/plugin-proposal-class-properties',
       ['@babel/plugin-proposal-private-methods', { 'loose': false }],
-      ["@babel/plugin-proposal-private-property-in-object", { "loose": false }],
+      ["@babel/plugin-proposal-private-property-in-object", { 'loose': false }],
       // Support remove different platform code.
       ['./scripts/platform-plugin', { platform }],
     ]
