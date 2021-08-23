88b13576da82325cdf9abd45890986eb50e956f1
chenrongyan.cry <chenrongyan.cry@alibaba-inc.com>
2021-08-23 16:44:27 +0800

    fix

--- packages/eslint-plugin-rax-runtime-miniapp/src/index.js
+++ packages/eslint-plugin-rax-runtime-miniapp/src/index.js
@@ -4,16 +4,7 @@
  */
 "use strict";
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
 const requireIndex = require("requireindex");
 
-//------------------------------------------------------------------------------
-// Plugin Definition
-//------------------------------------------------------------------------------
-
-
-// import all rules in lib/rules
+// import all rules in src/rules
 module.exports.rules = requireIndex(__dirname + "/rules");
