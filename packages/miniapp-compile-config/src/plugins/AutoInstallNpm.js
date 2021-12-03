04fff30bcecf41de9bb628c0d9c1a5b15dfd0661
逆葵 <xianyong.yxy@alibaba-inc.com>
2021-12-03 15:29:41 +0800

    feat(jsx2mp): not generate duplicate package.json in dev mode

--- packages/miniapp-compile-config/src/plugins/AutoInstallNpm.js
+++ packages/miniapp-compile-config/src/plugins/AutoInstallNpm.js
@@ -1,6 +1,40 @@
 const { autoInstallNpm } = require('miniapp-builder-shared');
-const { writeJSONSync } = require('fs-extra');
+const { writeJSONSync, existsSync, readJSONSync } = require('fs-extra');
 const { join, dirname } = require('path');
+
+function isObjectValueEqual(a, b) {
+  var aProps = Object.getOwnPropertyNames(a);
+  var bProps = Object.getOwnPropertyNames(b);
+
+  if (aProps.length != bProps.length) {
+    return false;
+  }
+
+  for (var i = 0; i < aProps.length; i++) {
+      var propName = aProps[i];
+      var propA = a[propName];
+      var propB = b[propName];
+      if ( propA !== propB) {
+        return false;
+      }
+  }
+  return true;
+}
+
+function shouldWritePackageJson (packageJsonPath, currentDependencies) {
+  if(!currentDependencies) {
+    return false;
+  }
+  if (!existsSync(packageJsonPath)) {
+    return true;
+  }
+  const oldDependencies = readJSONSync(packageJsonPath).dependencies;
+  if (isObjectValueEqual(currentDependencies, oldDependencies)) {
+    return false;
+  }
+  return true;
+}
+
 /**
  * Auto install npm
  */
@@ -14,19 +48,24 @@ module.exports = class AutoInstallNpmPlugin {
     compiler.hooks.done.tapAsync('AutoInstallNpmPlugin', async(stats, callback) => {
       const packageJsonFilePath = [];
       const distDir = stats.compilation.outputOptions.path;
+
       // Generate package.json
-      if (this.dependencies) {
-        writeJSONSync(join(distDir, 'package.json'), { dependencies: this.dependencies });
+      const writePath = join(distDir, 'package.json');
+      if(shouldWritePackageJson(writePath, this.dependencies)) {
+        writeJSONSync(writePath, { dependencies: this.dependencies });
         packageJsonFilePath.push('');
       }
       if (this.subPackages) {
         this.subPackages.forEach(({ dependencies = {}, source = '' }) => {
-          writeJSONSync(join(distDir, dirname(source), 'package.json'), { dependencies });
-          packageJsonFilePath.push(dirname(source));
+          const writePath = join(distDir, dirname(source), 'package.json');
+          if (shouldWritePackageJson(writePath, dependencies)) {
+            writeJSONSync(writePath, { dependencies });
+            packageJsonFilePath.push(dirname(source));
+          }
         });
       }
 
-      if (!this.autoInstall) {
+      if (!this.autoInstall || packageJsonFilePath.length === 0) {
         return callback();
       }
       await autoInstallNpm(callback, { distDir, packageJsonFilePath });
